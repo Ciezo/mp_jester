2c1daf1425270dfc375d4a1f2ab377ed77686143
Cloyd Van <abcd123.letters@gmail.com>
2022-07-16 12:03:53 +0800

    Update to data type of PK when INSERT into the Jester_DB
    PK of both tables are now alphanumeric
    It is based on the CodeGen.java I wrote which returns an item_code of alphanumeric key
    It is totally random and unique to prevent any anomalies of records into the database

--- src/sql/db/scripts/popu_jester_users-tbl.sql
+++ src/sql/db/scripts/popu_jester_users-tbl.sql
@@ -16,10 +16,10 @@ INSERT INTO jester_users (ID_auth, f_name, l_name, userName, user_pwd)
                 THESE ROW VALUES ARE CONSIDERED TO BE THE FIRST INITIALIZATION TO FILL UP THE DATABASE. 
                 THEY ARE MORE LIKE THE DEVELOPERS WHO ALREADY HAVE BACKDOOR ACCESS TO THE APP WITHOUT REGISTRATION
         */        
-        ROW(100, "Cloyd", "Secuya", "cloydvan", "12345"),
-        ROW(200, "Ron", "Relayo", "ron", "12345"),
-        ROW(300, "Karl", "Halcon", "karl", "12345"),
-        ROW(400, "Nadrin", "Caluya", "nadrin", "12345");
+        ROW("100", "Cloyd", "Secuya", "cloydvan", "12345"),
+        ROW("200", "Ron", "Relayo", "ron", "12345"),
+        ROW("300", "Karl", "Halcon", "karl", "12345"),
+        ROW("400", "Nadrin", "Caluya", "nadrin", "12345");
         
         /* New row values will be inserted, and they are considered to be the NEW USERS who registered the app. */
         /* After this, it is expected that the app itself will be the one creating and executing the SQL statements */
