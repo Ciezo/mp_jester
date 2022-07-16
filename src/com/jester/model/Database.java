fd0fe853f92a09bb73791d7770999d6539145ca7
Cloyd Van <abcd123.letters@gmail.com>
2022-07-16 12:12:01 +0800

    Trying to set up an interface based from ..music_handler
    This will be used later on to access the objects from the database as localized

--- src/com/jester/model/Database.java
+++ src/com/jester/model/Database.java
@@ -1,5 +1,52 @@
+/**
+    Author     : Cloyd Van S. Secuya
+    Filename   : Database.java
+    Package	   : com.jester.model;
+    Date of Creation : June 15, 2022
+    Description:
+        The Database.java interacts with the Model logic which implements methods that get 
+        the Music attributes and its assigned values either as objects and such.
+*/
+
+// PACKAGE SECTION
 package com.jester.model;
 
-public class Database {
+
+// IMPORT SECTION
+import java.util.ArrayList;
+import com.jester.model.music_handler.Interface_musichandle;
+import com.jester.model.music_handler.Music;
+
+
+public class Database implements Interface_musichandle {
+    @Override
+    public Music[] getMusicArrObj() {
+        
+        return null;
+    }
+
+    @Override
+    public ArrayList<Music> getMusicRecords() {
+        
+        return null;
+    }
+
+    @Override
+    public Music getMusicToPlay(String music_ID) {
+        
+        return null;
+    }
+
+    @Override
+    public void add_new_Music(Music music) {
+        
+        
+    }
+
+    @Override
+    public void playSound(String path) {
+        
+        
+    }
     
 }
